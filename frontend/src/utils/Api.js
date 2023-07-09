0cfda71a92d6863b7104a7cb5510cb8fa776e422
Anastasiia Klykova <qwerty03031998@yandex.ru>
2023-07-09 17:00:57 +0300

    bugs fixed

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -88,7 +88,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: 'http://localhost:3000',
   headers: {
     authorization: `Bearer ${localStorage.getItem("jwt")}`,
     'Content-Type': 'application/json'
